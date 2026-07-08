426b80c75a7c51362378cb96ac581d4383eb662f
Lucas <14808@holbertonstudents.com>
2026-07-08 12:02:21 +0000

    created function that prints 0-14

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14 ten times
+ *
+ * Return: nothing
+ */
+void more_numbers(void)
+{
+	int i;
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+			{
+				_putchar('1');
+				_putchar((i % 10) + '0');
+			}
+			else
+			{
+				_putchar(i + '0');
+			}
+		}
+
+		_putchar('\n');
+	}
+}

8d5e05e09e15713ff432d4a93e303acda56c715e
Lucas <14808@holbertonstudents.com>
2026-08-19 09:07:42 +0000

    adjusted program for betty errors

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -16,6 +16,76 @@ void close_fd(int fd)
 }
 
 /**
+ * open_source - opens file_from for reading
+ * @file_from: name of the file to open
+ *
+ * Return: the file descriptor on success
+ */
+int open_source(char *file_from)
+{
+	int fd;
+
+	fd = open(file_from, O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+			file_from);
+		exit(98);
+	}
+	return (fd);
+}
+
+/**
+ * open_dest - opens (creating/truncating if needed) file_to for writing
+ * @file_to: name of the file to open
+ *
+ * Return: the file descriptor on success
+ */
+int open_dest(char *file_to)
+{
+	int fd;
+
+	fd = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+	return (fd);
+}
+
+/**
+ * copy_content - copies the content of fd_from into fd_to
+ * @fd_from: file descriptor to read from
+ * @fd_to: file descriptor to write to
+ * @file_from: name of the source file (for error messages)
+ * @file_to: name of the destination file (for error messages)
+ */
+void copy_content(int fd_from, int fd_to, char *file_from, char *file_to)
+{
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
+
+	bytes_read = read(fd_from, buffer, BUFFER_SIZE);
+	while (bytes_read > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written == -1 || bytes_written != bytes_read)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			exit(99);
+		}
+		bytes_read = read(fd_from, buffer, BUFFER_SIZE);
+	}
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+			file_from);
+		exit(98);
+	}
+}
+
+/**
  * main - copies the content of a file to another file
  * @argc: number of arguments passed to the program
  * @argv: array of arguments passed to the program
@@ -25,8 +95,6 @@ void close_fd(int fd)
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to;
-	ssize_t bytes_read, bytes_written;
-	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
 	{
@@ -34,37 +102,10 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+	fd_from = open_source(argv[1]);
+	fd_to = open_dest(argv[2]);
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-
-	bytes_read = read(fd_from, buffer, BUFFER_SIZE);
-	while (bytes_read > 0)
-	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1 || bytes_written != bytes_read)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
-		bytes_read = read(fd_from, buffer, BUFFER_SIZE);
-	}
-
-	if (bytes_read == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+	copy_content(fd_from, fd_to, argv[1], argv[2]);
 
 	close_fd(fd_from);
 	close_fd(fd_to);
